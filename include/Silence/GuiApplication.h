012f3570707c00a45eb35721eab9345fa8949d7e
doom <1075101233@qq.com>
2022-11-29 23:53:01 +0900

    [update]disable copy

--- include/Silence/GuiApplication.h
+++ include/Silence/GuiApplication.h
@@ -2,6 +2,7 @@
 #define OPENGL_TUTORIAL_SILENCE_GUI_APPLICATION_H
 
 #include "Application.h"
+#include "Utility.h"
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -17,6 +18,10 @@ constexpr static const char* WINDOW_TITLE = "OpenGL Tutorial";
 class GuiApplication: public Application {
 public:
   GuiApplication(const std::string& title = WINDOW_TITLE);
+
+  SIL_DISABLE_COPY(GuiApplication)
+  
+  SIL_DEFAULT_MOVE(GuiApplication)
 
   ~GuiApplication();
 
